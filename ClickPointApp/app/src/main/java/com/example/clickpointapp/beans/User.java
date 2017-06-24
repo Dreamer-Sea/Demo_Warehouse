196629ba1ac699e5a4143d0e5364fafded30bdde
King <chenzipeng10@126.com>
2017-06-24 15:53:01 +0800

    一点点

---
+++ ClickPointApp/app/src/main/java/com/example/clickpointapp/beans/User.java
@@ -0,0 +1,26 @@
+package com.example.clickpointapp.beans;
+
+/**
+ * Created by King on 2017/6/24.
+ */
+
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
