12f24750f6e9f3eb027e13f59e34fe7e1948f49f
King <chenzipeng10@126.com>
2017-07-02 13:42:17 +0800

    添加图片下载功能

---
+++ WebMusicTest/app/src/main/java/com/example/webmusictest/threads/ReceiveJSON.java
@@ -0,0 +1,27 @@
+package com.example.webmusictest.threads;
+import com.show.api.ShowApiRequest;
+
+/**
+ * Created by King on 2017/6/19.
+ */
+
+public class ReceiveJSON extends Thread {
+
+    private String res;
+    private String url;
+
+    public ReceiveJSON(String url){
+        this.url = url;
+    }
+
+    public void run() {
+        String appid = "39525";
+        String secret = "7003fc66ab7443b3958e700508437360";
+        res = new ShowApiRequest(url, appid, secret)
+                .post();
+    }
+
+    public String  getRes() {
+        return res;
+    }
+}
